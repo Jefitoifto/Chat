f3625a8f3673487ba3f90b3634900d79c4acaddc
Jeferson Oliveira <81529357+Jefitoifto@users.noreply.github.com>
2021-05-09 22:40:31 -0300

    Update ClientSocket.java
    finalizado

--- src/chat/basic/ClientSocket.java
+++ src/chat/basic/ClientSocket.java
@@ -6,30 +6,43 @@ import java.net.Socket;
 import java.net.SocketAddress;
 
 public class ClientSocket {
+
     private final Socket socket;
     private final BufferedReader in;
     private final PrintWriter out;
-    
-    public ClientSocket(Socket socket) throws IOException{ /*O socket do lado cliente é usado para se conectar com o servior, e virse versa*/
-       this.socket = socket;
-       System.out.println("Cliente "+ socket.getRemoteSocketAddress()+" conectou");
-       this.in = new BufferedReader(
-       new InputStreamReader(socket.getInputStream()));
-       this.out = new PrintWriter(socket.getOutputStream(), true);
+
+    public ClientSocket(Socket socket) throws IOException {
+        /*O socket do lado cliente é usado para se conectar com o servior, e virse versa*/
+        this.socket = socket;
+        System.out.println("Cliente " + socket.getRemoteSocketAddress() + " conectou");
+        this.in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+        this.out = new PrintWriter(socket.getOutputStream(), true);
     }
-    public SocketAddress getRemoteSocketAddress(){
+
+    public SocketAddress getRemoteSocketAddress() {
         return socket.getRemoteSocketAddress();
     }
-    
-    public String getMessage(){
+
+    public void close() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            System.out.println("Erro ao fechar socket: " + e.getMessage());
+        }
+    }
+
+    public String getMessage() {
         try {
             return in.readLine();
-        } catch(IOException e){
+        } catch (IOException e) {
             return null;
         }
     }
-    
-    public boolean sendMsg(String msg){
+
+    public boolean sendMsg(String msg) {
         out.println(msg);
         return !out.checkError();
     }
