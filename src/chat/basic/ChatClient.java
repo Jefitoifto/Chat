d75a32258921966dbd51f4c450e139cc1fec87a6
Jeferson Oliveira <81529357+Jefitoifto@users.noreply.github.com>
2021-05-09 22:41:17 -0300

    Update ChatClient.java
    Finalizado

--- src/chat/basic/ChatClient.java
+++ src/chat/basic/ChatClient.java
@@ -1,49 +1,60 @@
 
 package chat.basic;
 
-
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+public class ChatClient implements Runnable {
 
-public class ChatClient {
-    private Socket clientSocket;
-    private final String SERVER_ADDRESS = "127.0.0.1";
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private ClientSocket clientSocket;
     private Scanner scanner;
-    private PrintWriter out;
-    
-    public ChatClient(){
+
+    public ChatClient() {
         scanner = new Scanner(System.in);
     }
-    
-    
-    public void start() throws IOException{
-        clientSocket = new Socket(SERVER_ADDRESS, ChatServer.PORT);
-        this.out = new PrintWriter(clientSocket.getOutputStream(), true);
-        
-        clientSocket.getOutputStream();
-        messageLoop();
-        System.out.println("Cliente conectado ao servidor em "+ SERVER_ADDRESS + ":"+ ChatServer.PORT);
+
+    public void start() throws IOException {
+        try {
+            clientSocket = new ClientSocket(
+                    new Socket(SERVER_ADDRESS, ChatServer.PORT));
+
+            System.out.println("Cliente conectado ao servidor em " + SERVER_ADDRESS + ":" + ChatServer.PORT);
+            new Thread(this).start();
+            messageLoop();
+        } finally {
+            clientSocket.close();
+        }
     }
-    
-    private void messageLoop() throws IOException{
+
+    @Override
+    public void run() {
+        String msg;
+        while ((msg = clientSocket.getMessage()) != null) {
+            System.out.println("Msg recebida do servidor: \n"+msg);
+        }
+    }
+
+    private void messageLoop() throws IOException {
         String msg;
         do {
             System.out.print("Digite uma mensagem ou sair para finalizar: ");
             msg = scanner.nextLine();
-            out.println(msg);  
-        } while(!msg.equalsIgnoreCase("sair"));
+            clientSocket.sendMsg(msg);
+            clientSocket.sendMsg(msg);
+
+        } while (!msg.equalsIgnoreCase("sair"));
     }
-    
+
     public static void main(String[] args) {
-        
+
         try {
-            ChatClient client = new ChatClient(); /*Operação bloqueante*/
+            ChatClient client = new ChatClient();
+            /*Operação bloqueante*/
             client.start();
         } catch (IOException ex) {
-            System.out.println("Erro ao iniciar o cliente: "+ex.getMessage());
+            System.out.println("Erro ao iniciar o cliente: " + ex.getMessage());
         }
         System.out.println("Cliente finaliado!");
     }
